34f8879a0c1a7908af9f9af52491f93c87131d24
sunknight <sunknight@163.com>
2026-08-25 00:02:40 +0800

    termstep 1.3.1

--- Casks/termstep.rb
+++ Casks/termstep.rb
@@ -1,6 +1,6 @@
 cask "termstep" do
-  version "1.3.0"
-  sha256 "ab76e8b6fffef948d8d5250736e29f2bd45328c9758da66d07f7a53a28093c0a"
+  version "1.3.1"
+  sha256 "d4b8cc2a5d082ae3e477b3a8ded355794c82e5dd1008bdc5fd8664b00a831493"
 
   url "https://github.com/sunknight/termstep/releases/download/v#{version}/TermStep_#{version}_universal.dmg"
   name "TermStep"
